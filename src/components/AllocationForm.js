f630505c543676b8a7fec470da3d023a75d686b1
amrkhaled617 <amrkhaled54321@outlook.com>
2024-07-19 23:47:39 +0300

    commit

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,7 +58,7 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                    <p style={{marginLeft: "30px" ,marginRight: "-25px", display: "inline-block"}}>£</p>
                     <input
                         required='required'
                         type='number'
